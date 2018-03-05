f2b194b8b0ad97d25158681e143f6447accd9d97
Володимир Смульський <smulia18ua@gmail.com>
2018-03-05 13:00:57 +0200

    Created project & draw one circle with lable.

---
+++ Graps (AI Lab_1)/Graps (AI Lab_1)/DrawGraphsUIView.swift
@@ -0,0 +1,31 @@
+//
+//  DrawGraphsUIView.swift
+//  Graps (AI Lab_1)
+//
+//  Created by Володимир Смульський on 3/5/18.
+//  Copyright © 2018 Володимир Смульський. All rights reserved.
+//
+
+import UIKit
+
+class DrawGraphsUIView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        let r = CGRect(x: 10, y: 10, width: 50, height: 50)
+        
+        let context = UIGraphicsGetCurrentContext()
+        context?.move(to: r.origin)
+        context?.addEllipse(in: r)
+        context?.fillPath()
+        
+        //         let b = UIBezierPath(roundedRect: r, cornerRadius: r.height*0.5)
+        //        b.fill()
+        let s = NSString(string: "asdf")
+        s.draw(at: CGPoint(x:120, y:20), withAttributes: nil)
+    }
+ 
+
+}
---
+++ Graps (AI Lab_1)/Graps (AI Lab_1)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Graps (AI Lab_1)
+//
+//  Created by Володимир Смульський on 3/5/18.
+//  Copyright © 2018 Володимир Смульський. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
